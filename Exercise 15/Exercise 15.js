82bbb726b97ee4e988108fb179c0e6abae9e6140
MykaelaGarcia <98378436+Mykaela-Garcia@users.noreply.github.com>
2022-05-13 21:48:59 +0800

    Add files via upload

---
+++ Exercise 15/Exercise 15.js
@@ -0,0 +1,20 @@
+var myName="Mykaela Regine Garcia"
+var myAge="19"
+var myPhoneNumber="09123456789"
+var myEmail="200707778@s.ubaguio.edu"
+var myAddress="University of Baguio"
+var myProgram="Bachelor of Science in Computer Science"
+var myID="20070778"
+var myYear="1st Year"
+var mySection="IAA1"
+
+console.log('Hello!');
+console.log('First Name: '+ myName);
+console.log('Age: '+ myAge);
+console.log('Phone Number '+ myPhoneNumber);
+console.log('Email: '+ myEmail);
+console.log('Mailing Address: '+ myAddress);
+console.log('Program: '+ myProgram);
+console.log('ID: '+ myID);
+console.log('Year: '+ myYear);
+console.log('Section: '+ mySection);
